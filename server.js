105b5be48eb0351cceafbfacbd194e3decab9195
Braydon Nelson <braydon.nelson@outlook.com>
2020-02-13 17:58:30 -0700

    added functionality for delete

--- server.js
+++ server.js
@@ -8,33 +8,33 @@ app.use(express.static('public'))
 
 var PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '/public/index.html');
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/notes', (req, res) => {
-    res.status(200).sendFile(__dirname + '/public/notes.html');
+app.get('/notes', (request, response) => {
+    response.sendFile(__dirname + '/public/notes.html');
 });
 
-app.get('/api/notes', (req, res) => {
+app.get('/api/notes', (request, response) => {
     try {
         fs.readFile(__dirname + '/db/db.json', 'utf-8', (err, data) => {
             if (err) {
                 throw Error(err);
-            }            
+            }
             const jsonData = JSON.parse(data);
-            res.status(200).send(jsonData);
+            response.send(jsonData);
         })
     } catch (err) {
         console.error(err);
-        res.status(404).send(`Could not find a note to load.`);
+        response.send(`Could not find a note to load.`);
     }
 });
 
-app.post('/api/notes', (req, res) => {
+app.post('/api/notes', (request, response) => {
     // Parse incoming request message
     let message = '';
-    req.on('data', data => {
+    request.on('data', data => {
         message += data.toString();
     }).on('end', () => {
         const newNote = querystring.parse(message);
@@ -47,7 +47,7 @@ app.post('/api/notes', (req, res) => {
 
                 data = JSON.parse(data);
                 // Set new notes id
-                newNote.id = data.length;
+                newNote.id = data.length + 1;
                 data.push(newNote);
 
                 fs.writeFile(__dirname + '/db/db.json', JSON.stringify(data), err => {
@@ -55,13 +55,36 @@ app.post('/api/notes', (req, res) => {
                     console.log('Message Saved.')
                 });
             });
-            res.send(newNote);
+            response.send(newNote);
         } else {
             throw new Error('Something went wrong.');
         }
     });
 })
 
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id;
+    fs.readFile(__dirname + '/db/db.json', 'utf-8', (err, notes) => {
+        if (err) {
+            throw err;
+        }
+        console.log(`found a file now going to delete it`);
+        notes = JSON.parse(notes);
+
+        for (let i = 0; i < notes.length; i++) {
+            if (notes[i].id === parseInt(id)) {
+                notes.splice(i, 1);
+            }
+        }
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(notes), err => {
+            if (err) {
+                throw err;
+            }
+            console.log(`Message Deleted.`);
+        })
+    })
+    response.send(`Message Deleted`)
+})
 
 
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`))
